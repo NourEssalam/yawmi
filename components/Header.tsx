7c8013f83ebf3a4faf0c21fb8d4c2e29a42a72cb
NourEssalam <werteni11thug@gmail.com>
2024-07-17 23:12:41 +0100

    make the header responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,22 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex sticky top-0 z-10 items-center justify-between  px-20  w-[800px] bg-red-200 mx-auto my-0">
+    <header className="flex sticky top-0 z-10 items-center justify-between md:py-5 px-5 md:px-24 max-w-7xl mx-auto  bg-gray-200 ">
       <Link href="/" className="text-2xl font-bold">
-        Todo App
+        Yawmi
       </Link>
-      <div className="flex space-x-4">
-        <Link href="/login" className="hover:underline">
+      <div className="flex flex-col md:flex-row items-center ">
+        <Link
+          href="/auth/login"
+          className="hover:underline   font-semibold text-black  visited:border-dotted md:px-2 py-1"
+        >
           Login
         </Link>
-        <span>|</span>
-        <Link href="/sign-in" className="hover:underline">
+        {/* <span>|</span> */}
+        <Link
+          href="/auth/signin"
+          className="hover:underline font-semibold text-black  visited:underline border-t md:border-l md:border-t-0 md:px-2 border-black py-1"
+        >
           Sign-In
         </Link>
       </div>
